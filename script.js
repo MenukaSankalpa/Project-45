feb126a9180c73886f7efecc4d33763c69bfe42b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-01 20:07:17 +0530

    change snake and food positon js file

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const playBoard = document.querySelector(".play-board");
 
-let foodX = 13, foodY= 10;
+let foodX , foodY;
+let snakeX = 5, snakeY = 10;
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const changeDirection = (e) => {
+    console.log(e);
+}
 
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
 }
+changeFoodPosition();
 initGame();
+document.addEventListener("keydown", changeDirection);
